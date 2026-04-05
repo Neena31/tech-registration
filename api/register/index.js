dddb71c748cd305b1c6db81ca7c7718990533449
Neena Mandanna <neenamandanna@Neenas-MacBook-Air.local>
2026-04-05 20:18:50 +0530

    Fix db import path after moving API function

--- api/register/index.js
+++ api/register/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config({ path: ".env.local" });
-const pool = require('../db');
+const pool = require('../../db');
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
